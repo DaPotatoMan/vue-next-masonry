572fb46c86fd6f89d8d1ed76b186604ac06ba625
Fahadul Islam <44726129+DaPotatoMan@users.noreply.github.com>
2021-05-23 19:29:14 +0600

    feat: Create postbuild script, resolves #1

---
+++ build/build.ts
@@ -0,0 +1,24 @@
+const { copyFile, remove, writeFile } = require('fs-extra');
+
+async function handlePackageJSON() {
+   const keys = ['name', 'version', 'author', 'respository'];
+   const package = require('./package.json');
+   const origPackage = require('../package.json');
+
+   // Copy keywords
+   keys.forEach((key) => {
+      package[key] = origPackage[key];
+   });
+
+   await writeFile('./dist/package.json', JSON.stringify(package));
+}
+
+async function init() {
+   // Copy files
+   await copyFile('README.md', './dist/README.md');
+
+   // Create package.json
+   await handlePackageJSON();
+}
+
+init();
